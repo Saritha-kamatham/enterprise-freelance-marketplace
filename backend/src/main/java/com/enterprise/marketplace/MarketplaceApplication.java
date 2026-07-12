235986c2522a2214d3061f04d3869a3fa79546a2
Saritha Kamatham <sarithakamatham2006@gmail.com>
2026-07-12 17:55:37 +0530

    Clean up printHash debug code

--- backend/src/main/java/com/enterprise/marketplace/MarketplaceApplication.java
+++ backend/src/main/java/com/enterprise/marketplace/MarketplaceApplication.java
@@ -1,8 +1,11 @@
 package com.enterprise.marketplace;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
